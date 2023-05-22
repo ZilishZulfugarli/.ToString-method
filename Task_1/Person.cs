bee607e1419707b19f16918ea9d09919e2443e33
ZilishZulfugarli <2002zilis@gmail.com>
2023-05-23 01:39:47 +0400

    Add project files.

---
+++ Task_1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        public Person (string name, string surname, int age, double weight, double height)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return $" Name: {Name} , Surname: {Surname} , Age:  {Age} , Weight:  {Weight} kg , Height: {Height} metres";
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person ("Niyazi" , "Babayev" , 20 , 75 , 1.75) ;
+
+
+            Console.WriteLine(person);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
